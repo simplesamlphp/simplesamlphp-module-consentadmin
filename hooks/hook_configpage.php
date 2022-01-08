18a46f5883cc0db5cb968867e4cb9d572c7ac79b
Tim van Dijen <tvdijen@gmail.com>
2022-01-08 18:38:50 +0100

    Add some use-statements

--- hooks/hook_configpage.php
+++ hooks/hook_configpage.php
@@ -1,15 +1,20 @@
 <?php
+
+use SimpleSAML\Locale\Translate;
+use SimpleSAML\Module;
+use SimpleSAML\XHTML\Template;
+
 /**
  * Hook to add the consentAdmin module to the config page.
  *
  * @param \SimpleSAML\XHTML\Template $template The template that we should alter in this hook.
  * @return void
  */
-function consentAdmin_hook_configpage(\SimpleSAML\XHTML\Template &$template): void
+function consentAdmin_hook_configpage(Template &$template): void
 {
     $template->data['links']['consentAdmin'] = [
-        'href' => SimpleSAML\Module::getModuleURL('consentAdmin/consentAdmin.php'),
-        'text' => \SimpleSAML\Locale\Translate::noop('Consent administration'),
+        'href' => Module::getModuleURL('consentAdmin/consentAdmin.php'),
+        'text' => Translate::noop('Consent administration'),
     ];
     $template->getLocalization()->addModuleDomain('consentAdmin');
 }
